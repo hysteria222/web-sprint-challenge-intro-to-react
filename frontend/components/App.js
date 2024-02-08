66850515eb1080e6c5b56d0f80490946ebbd5cdb
HYSTERIA <129130014+hysteria222@users.noreply.github.com>
2024-02-08 08:58:25 -0600

    pushing to save changes.

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,61 +1,62 @@
-import React, {useState, useEffect} from 'react'
-import axios from 'axios'
-import Character from './Character'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Character from "./Character";
 
-const urlPlanets = 'http://localhost:9009/api/planets'
-const urlPeople = 'http://localhost:9009/api/people'
+const urlPlanets = "http://localhost:9009/api/planets";
+const urlPeople = "http://localhost:9009/api/people";
 
 function App() {
   // ❗ Create state to hold the data from the API
 
-  const [planetData, setPlanetData] = useState('')
-  const [peopleData, setPeopleData] = useState('')
-
+  const [data, setData] = useState([]);
 
   // ❗ Create effects to fetch the data and put it in state
 
   useEffect(() => {
     axios
-      .get('http://localhost:9009/api/planets')
-      .then(res => {
-        // console.log(res.data);
-        setPlanetData(res.data.name)
+      .get("http://localhost:9009/api/planets")
+      .then((resp1) => {
+        axios.get("http://localhost:9009/api/people").then((resp2) => {
+          const planetData = resp1.data;
+          const peopleData = resp2.data;
+          const finalData = peopleData.map((person) => {
+            const homeworldInfo = planetData.find(
+              (planet) => person.homeworld === planet.id
+            );
+            return {
+              ...person,
+              homeworld: homeworldInfo,
+            };
+          });
+          console.log(finalData);
+          setData(finalData);
+        });
       })
-      .catch(err => console.log(err.message));
-  }, [])
-
-  useEffect(() => {
-    axios
-      .get('http://localhost:9009/api/people')
-      .then(res => {
-        // console.log(res.data);
-        setPeopleData(res.data.name)
-      })
-      .catch(err => console.log(err.message));
-  }, [])
-
-
- useEffect(() => {
-  
-}, [])
-  
+      .catch((err) => console.log(err.message));
+  }, []);
 
   return (
     <div>
       <h2>Star Wars Characters</h2>
-      <p>See the README of the project for instructions on completing this challenge</p>
-        <div className='character-card'>
-        {/* ❗ Map over the data in state, rendering a Character at each iteration */
-          
+      <p>
+        See the README of the project for instructions on completing this
+        challenge
+      </p>
+      <div>
+        {
+          /* ❗ Map over the data in state, rendering a Character at each iteration */
+          data.map((item, i) => {
+            return (
+            <Character key={item.id} {...data[i]}/>
+            )
+          })
         }
-        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
 
 // ❗ DO NOT CHANGE THE CODE  BELOW
-if (typeof module !== 'undefined' && module.exports) module.exports = App
-
-
+if (typeof module !== "undefined" && module.exports) module.exports = App;
--- frontend/components/Character.js
+++ frontend/components/Character.js
@@ -1,23 +1,33 @@
-import React, { useState, useEffect } from 'react'
-
-function Character() { // ❗ Add the props
+import React, { useState, useEffect } from "react";
+function Character(props) {
+  // ❗ Add the props
   // ❗ Create a state to hold whether the homeworld is rendering or not
 
-  const [homeWorldShow, homeWorldHide] = useState('')
+  const [homeWorld, setHomeWorld] = useState(false);
+
   // ❗ Create a "toggle" click handler to show or remove the homeworld
 
-  useEffect(() => {
-    onclick={homeWorldHide}
-  }, homeWorldShow)
+  const handleClick = () => {
+    if (homeWorld != true) {
+      setHomeWorld(true);
+    } else if (homeWorld === true) {
+      setHomeWorld(false);
+    }
+  };
+
   return (
-    <div>
-      {/* Use the same markup with the same attributes as in the mock */
-      
-      }
+    <div onClick={handleClick} className="character-card">
+      <h3 className="character-name">{props.name}</h3>
+      <p>
+        {homeWorld ? (
+          <>
+            Planet:{" "}
+            <span className="character-planet">{props.homeworld.name}</span>
+          </>
+        ) : null}
+      </p>
     </div>
-  )
+  );
 }
 
-export default Character
-
-
+export default Character;
